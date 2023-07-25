9d2c4d2c4dda7be35ed9e875573a488e039bfddf
Mahesh <maheshdsu86@gmail.com>
2023-07-25 22:48:06 +0530

    GS Receive

---
+++ template/demo_1/src/app/screens/GSreceive.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const GSreceive = () => {
+  return (
+    <>
+        <div className="row">
+          <div className="col-md-4 grid-margin stretch-card">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">Customer Details</h4>
+                <form className="forms-sample">
+
+                </form>
+                </div>
+             </div>
+         </div>
+        </div>
+    </>
+  )
+}
+
+export default GSreceive
